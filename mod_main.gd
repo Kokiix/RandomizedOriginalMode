84dff84eeb5bda482af089d6d4d2c0b8eec26d12
kenneth <kokiidoki@gmail.com>
2026-08-01 10:23:22 -0400

    randomize default playlist

--- mod_main.gd
+++ mod_main.gd
@@ -1,10 +1,17 @@
 extends Node
 
 const MOD_DIR := "koki-RandomizedClassicMode"
-const LOG_NAME := "koki-RandomizedClassicMode:Main"
 
 var mod_dir_path := ""
 
 func _init() -> void:
-	ModLoaderLog.info("Init", LOG_NAME)
 	mod_dir_path = ModLoaderMod.get_unpacked_dir().path_join(MOD_DIR)
+	install_script_hooks()
+	
+func install_script_hooks() -> void:
+	pass
+	ModLoaderMod.add_hook(randomize_default_playlist, "res://scripts/scenes/game/game.gd", "generate_session_playlist")
+
+func randomize_default_playlist(chain: ModLoaderHookChain) -> void:
+	Globals.default_playlist.shuffle()
+	chain.execute_next()
