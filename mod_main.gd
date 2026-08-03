3d56ca7038531b06541c3e5ead4520c66951af89
kenneth <kokiidoki@gmail.com>
2026-08-03 16:24:28 -0400

    chore: move out of src because of required file structure in editor

--- mod_main.gd
+++ mod_main.gd
@@ -10,11 +10,8 @@ static func Log(msg: Variant, prefix: String = "DEBUG") -> void:
 func _init() -> void:
 	mod_dir_path = ModLoaderMod.get_unpacked_dir().path_join(MOD_DIR)
 	install_script_hooks()
+	Log("sldkfj")
 	
 func install_script_hooks() -> void:
-	ModLoaderMod.add_hook(randomize_default_playlist, "res://scripts/scenes/game/game.gd", "generate_session_playlist")
-
-func randomize_default_playlist(chain: ModLoaderHookChain) -> void:
-	#Log("running hook")
-	Globals.default_playlist.shuffle()
-	chain.execute_next()
+	#ModLoaderMod.add_hook(randomize_default_playlist, "res://scripts/scenes/game/game.gd", "generate_session_playlist")
+	pass
