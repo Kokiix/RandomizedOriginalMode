c14572ccf6c68c1d414200964f905c628ebdcafa
kenneth <kokiidoki@gmail.com>
2026-08-03 20:01:44 -0400

    feat: randomization toggle in custom mode

--- mod_main.gd
+++ mod_main.gd
@@ -1,17 +1,16 @@
 extends Node
+class_name PlaylistRandomizerMod
 
 const MOD_DIR := "koki-PlaylistRandomizer"
 
-var mod_dir_path := ""
+static var mod_dir_path := ""
 
 static func Log(msg: Variant, prefix: String = "DEBUG") -> void:
 	print_rich("[color=purple]%s RandomizedClassicMode: %s[/color]" % [prefix, msg])
 
 func _init() -> void:
-	mod_dir_path = ModLoaderMod.get_unpacked_dir().path_join(MOD_DIR)
-	install_script_hooks()
-	Log("sldkfj")
+	mod_dir_path = ModLoaderMod.get_unpacked_dir().path_join(MOD_DIR) + "/src"
 	
-func install_script_hooks() -> void:
-	#ModLoaderMod.add_hook(randomize_default_playlist, "res://scripts/scenes/game/game.gd", "generate_session_playlist")
-	pass
+	ModLoaderMod.install_script_extension(mod_dir_path + "/randomize_button/inject_button.gd")
+	
+	ModLoaderMod.add_hook(GeneratePlaylistHook.on_generate, "res://scripts/scenes/game/game.gd", "generate_session_playlist")
